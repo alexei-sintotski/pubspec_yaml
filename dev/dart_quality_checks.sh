cb97ad3a512b29886e91bcb04a2b6ed834f2e864
Alexei Sintotski <alexei.sintotski@gmail.com>
2019-11-30 16:10:11 +0100

    Unit testing is enabled in dart_quality_checks.sh

--- dev/dart_quality_checks.sh
+++ dev/dart_quality_checks.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -ex
 
 dartanalyzer --fatal-hints .
+pub run test
 pub run dependency_validator --ignore=functional_data_generator,sum_types_generator,test_coverage
 dev/format_dart_code.sh --set-exit-if-changed
 dev/generate_code.sh
