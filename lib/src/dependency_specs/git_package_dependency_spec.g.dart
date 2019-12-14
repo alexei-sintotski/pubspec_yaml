8f8978c6699597dfbe1f66d5625a08b679d4e1cb
Alexei Sintotski <alexei.sintotski@gmail.com>
2019-12-14 12:56:42 +0100

    Support for Git package dependency specifications

---
+++ lib/src/dependency_specs/git_package_dependency_spec.g.dart
@@ -0,0 +1,57 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'git_package_dependency_spec.dart';
+
+// **************************************************************************
+// FunctionalDataGenerator
+// **************************************************************************
+
+abstract class $GitPackageDependencySpec {
+  String get package;
+  String get url;
+  Optional<String> get ref;
+  Optional<String> get path;
+  const $GitPackageDependencySpec();
+  GitPackageDependencySpec copyWith({String package, String url, Optional<String> ref, Optional<String> path}) =>
+      GitPackageDependencySpec(
+          package: package ?? this.package, url: url ?? this.url, ref: ref ?? this.ref, path: path ?? this.path);
+  String toString() => "GitPackageDependencySpec(package: $package, url: $url, ref: $ref, path: $path)";
+  bool operator ==(dynamic other) =>
+      other.runtimeType == runtimeType &&
+      package == other.package &&
+      url == other.url &&
+      ref == other.ref &&
+      path == other.path;
+  @override
+  int get hashCode {
+    var result = 17;
+    result = 37 * result + package.hashCode;
+    result = 37 * result + url.hashCode;
+    result = 37 * result + ref.hashCode;
+    result = 37 * result + path.hashCode;
+    return result;
+  }
+}
+
+class GitPackageDependencySpec$ {
+  static final package =
+      Lens<GitPackageDependencySpec, String>((s_) => s_.package, (s_, package) => s_.copyWith(package: package));
+  static final url = Lens<GitPackageDependencySpec, String>((s_) => s_.url, (s_, url) => s_.copyWith(url: url));
+  static final ref =
+      Lens<GitPackageDependencySpec, Optional<String>>((s_) => s_.ref, (s_, ref) => s_.copyWith(ref: ref));
+  static final path =
+      Lens<GitPackageDependencySpec, Optional<String>>((s_) => s_.path, (s_, path) => s_.copyWith(path: path));
+}
+
+// ignore_for_file: always_put_required_named_parameters_first
+// ignore_for_file: always_require_non_null_named_parameters
+// ignore_for_file: annotate_overrides
+// ignore_for_file: avoid_annotating_with_dynamic
+// ignore_for_file: avoid_classes_with_only_static_members
+// ignore_for_file: non_constant_identifier_names
+// ignore_for_file: join_return_with_assignment
+// ignore_for_file: prefer_asserts_with_message
+// ignore_for_file: prefer_expression_function_bodies
+// ignore_for_file: prefer_single_quotes
+// ignore_for_file: sort_constructors_first
+// ignore_for_file: type_annotate_public_apis
---
+++ test/dependency_specs/git_package_dependency_spec_test.dart
@@ -0,0 +1,138 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 Alexei Sintotski
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+import 'package:pubspec_yaml/pubspec_yaml.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('given pubspec.yaml with Git package dependency', () {
+    final pubspecYaml = PubspecYaml.loadFromYamlString(pubspecYamlWithGitPackageDependency);
+
+    group('$PubspecYaml.loadFromYamlString', () {
+      test('produces object with single dependency', () {
+        expect(pubspecYaml.dependencies.length, 1);
+      });
+      test('produces object with GitPackageDependencySpec', () {
+        expect(pubspecYaml.dependencies.first.iswitcho(git: (p) => p, otherwise: () => null), isNotNull);
+      });
+      test('produces object with correct dependency name', () {
+        expect(pubspecYaml.dependencies.first.package(), dependency);
+      });
+      test('produces object with correct url', () {
+        expect(pubspecYaml.dependencies.first.iswitcho(git: (p) => p.url, otherwise: () => ''), url);
+      });
+      test('produces object without custom fields', () {
+        expect(pubspecYaml.customFields, isEmpty);
+      });
+    });
+
+    group('$PubspecYaml.toYamlString', () {
+      test('produces string equivalent to the input', () {
+        expect(pubspecYaml.toYamlString(), pubspecYamlWithGitPackageDependency);
+      });
+    });
+  });
+
+  group('given pubspec.yaml with Git package dependency with ref specified', () {
+    final pubspecYaml = PubspecYaml.loadFromYamlString(pubspecYamlWithGitRefSpecification);
+
+    group('$PubspecYaml.loadFromYamlString', () {
+      test('produces object with correct dependency name', () {
+        expect(pubspecYaml.dependencies.first.package(), dependency);
+      });
+      test('produces object with correct url', () {
+        expect(pubspecYaml.dependencies.first.iswitcho(git: (p) => p.url, otherwise: () => ''), url);
+      });
+      test('produces object with correct ref', () {
+        expect(pubspecYaml.dependencies.first.iswitcho(git: (p) => p.ref.valueOr(() => ''), otherwise: () => ''), ref);
+      });
+      test('produces object without path', () {
+        expect(pubspecYaml.dependencies.first.iswitcho(git: (p) => p.path.hasValue, otherwise: () => true), isFalse);
+      });
+    });
+
+    group('$PubspecYaml.toYamlString', () {
+      test('produces string equivalent to the input', () {
+        expect(pubspecYaml.toYamlString(), pubspecYamlWithGitRefSpecification);
+      });
+    });
+  });
+
+  group('given pubspec.yaml with Git package dependency with path specified', () {
+    final pubspecYaml = PubspecYaml.loadFromYamlString(pubspecYamlWithPathSpecification);
+
+    group('$PubspecYaml.loadFromYamlString', () {
+      test('produces object with correct path', () {
+        expect(
+            pubspecYaml.dependencies.first.iswitcho(
+              git: (p) => p.path.valueOr(() => ''),
+              otherwise: () => '',
+            ),
+            path);
+      });
+      test('produces object without ref', () {
+        expect(pubspecYaml.dependencies.first.iswitcho(git: (p) => p.ref.hasValue, otherwise: () => true), isFalse);
+      });
+    });
+
+    group('$PubspecYaml.toYamlString', () {
+      test('produces string equivalent to the input', () {
+        expect(pubspecYaml.toYamlString(), pubspecYamlWithPathSpecification);
+      });
+    });
+  });
+}
+
+const dependency = 'kittens';
+const url = 'git://github.com/munificent/kittens.git';
+const pubspecYamlWithGitPackageDependency = '''
+name: pubspec_yaml
+
+dependencies:
+  $dependency:
+    git: "$url"
+''';
+
+const ref = 'some-branch';
+const pubspecYamlWithGitRefSpecification = '''
+name: pubspec_yaml
+
+dependencies:
+  $dependency:
+    git:
+      url: "$url"
+      ref: "$ref"
+''';
+
+const path = 'path/to/kittens';
+const pubspecYamlWithPathSpecification = '''
+name: pubspec_yaml
+
+dependencies:
+  $dependency:
+    git:
+      url: "$url"
+      path: $path
+''';
